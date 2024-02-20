ad5db7219b57a8d90c61880a42c3c02182e11008
Gulnaz A. <akhmetovagrm@gmail.com>
2024-02-20 19:52:15 +0500

    Added func description

---
+++ db.py
@@ -0,0 +1,108 @@
+import os
+
+import psycopg2
+import sqlalchemy 
+from sqlalchemy.orm import sessionmaker
+
+from model import Users, Favorites, Blacklist
+
+
+
+def create_delete_db(name: str, command: str, if_: str):
+    """
+    Функция для удаления/создания Базы данных
+    """
+    conn_ = psycopg2.connect(database=os.getenv('LOGIN'), 
+                             user=os.getenv('LOGIN'), 
+                             password=os.getenv('PASSWORD'))
+    conn_.autocommit = True
+    with conn_.cursor() as cur:
+        sql = f"""{command} DATABASE {if_} {name}"""
+        print(sql)
+        cur.execute(sql)
+    conn_.close()    
+
+
+
+
+def create_connection_DB():
+    """
+    Функция для создания объекта-движка, для соединения с БД
+    """
+    
+    engine = sqlalchemy.create_engine(
+        f"{os.getenv('DRIVER')}://"
+        f"{os.getenv('LOGIN')}:{os.getenv('PASSWORD')}@"
+        f"{os.getenv('HOST')}:{os.getenv('PORT')}/{os.getenv('NAME_DB')}", echo=False
+    )
+    return engine
+
+
+
+"""Создание объекта-сессии"""
+Session = sessionmaker(bind=create_connection_DB())
+session = Session()
+
+
+
+def check_users(candidate_id: int):
+    """
+    Функция для проверки пользователя в БД (Users)
+    """
+    checking_user = session.query(Users).filter_by(owner_id=candidate_id).first()
+    return checking_user is None
+
+
+
+def add_new_user(owner_id: str, first_name: str, last_name: str, user_link: str):
+    """
+    Метод добавления пользователей в БД (Users)
+    """
+    users = Users(owner_id=owner_id, 
+                  first_name=first_name, 
+                  last_name=last_name, 
+                  user_link=user_link)
+    session.add(users)
+    session.commit()
+
+
+
+def add_favorite(user: dict, candidate: dict):
+    """
+    Метод для добавления пользователя в БД (Favorites)
+    """
+    user_id = session.query(Users).filter_by(owner_id=user['owner_id']).first()
+    candidate_id = session.query(Users).filter_by(owner_id=candidate['owner_id']).first()
+    new_favorite_user = Favorites(user_id=user_id.user_id, 
+                                  favorite_user_id=candidate_id.user_id)
+    session.add(new_favorite_user) 
+    session.commit()
+
+
+
+def add_black_list(user: dict, candidate: dict):
+    """
+    Метод для добавления пользователя в БД (Blacklist)
+    """    
+    user_id = session.query(Users).filter_by(owner_id=user['owner_id']).first()
+    candidate_id = session.query(Users).filter_by(owner_id=candidate['owner_id']).first()
+    new_ignored_user = Blacklist(user_id=user_id.user_id, 
+                                  black_list_id=candidate_id.user_id)
+    session.add(new_ignored_user) 
+    session.commit()
+
+
+
+
+def show_fav_list(user: dict):
+    """
+    Метод для извлечения информации о пользователях с БД (Favorites)
+    """      
+    filtered = session.query(Users.first_name, Users.last_name, Users.user_link).join(
+        Favorites, (Favorites.favorite_user_id == Users.user_id == user['owner_id'])).all()
+    result = []
+    for item in filtered:
+        result.append(item)
+    return result    
+
+
--- main.py
+++ main.py
@@ -1,75 +1,23 @@
 import os
 
-import psycopg2
-import sqlalchemy
-from sqlalchemy.orm import sessionmaker
-
-from model import Users, Photos, Favorites, Blacklist, create_table
+from db import create_connection_DB, create_delete_db, session
+from model import create_table
+from class_bot import Bot
 
 
 
-def create_delete_db(name, command, if_):
-    """Функция для создании Базы данных"""
 
-    conn_ = psycopg2.connect(database=os.getenv('LOGIN'), user=os.getenv('LOGIN'), password=os.getenv('PASSWORD'))
-    conn_.autocommit = True
-    with conn_.cursor() as cur:
-        sql = f"""{command} DATABASE {if_} {name}"""
-        print(sql)
-        cur.execute(sql)
-    conn_.close()    
+if __name__ == '__main__':
 
-# create_delete_db(name=os.getenv('NAME_DB'), command='DROP', if_='IF EXISTS')
-# create_delete_db(name=os.getenv('NAME_DB'), command='CREATE', if_='')  
-
+    create_delete_db(name=os.getenv('NAME_DB'), 
+                    command='DROP', 
+                    if_='IF EXISTS')
+    create_delete_db(name=os.getenv('NAME_DB'), 
+                    command='CREATE', 
+                    if_='')  
+      
+    create_table(create_connection_DB())
+    session.close()
     
-
-def create_connection_DB():
-    """Функция для создания объекта-движка, для соединения с БД"""
-    
-    engine = sqlalchemy.create_engine(
-        f"{os.getenv('DRIVER')}://"
-        f"{os.getenv('LOGIN')}:{os.getenv('PASSWORD')}@"
-        f"{os.getenv('HOST')}:{os.getenv('PORT')}/{os.getenv('NAME_DB')}", echo=False
-    )
-    return engine
-
-Session = sessionmaker(bind=create_connection_DB())
-session = Session()
-
-create_table(create_connection_DB())
-    
-
-
-def add_new_user(user):
-    users = Users(**user)
-    session.add(users)
-    session.commit()
-
-
-def check_users(candidate_id):
-    # проверка на то, есть ли пользователь в БД
-    checking_user = session.query(Users).filter_by(owner_id=candidate_id).first()
-    return checking_user is None
-
-
-
-def add_favorite(user, candidate):
-    user_id = session.query(Users).filter_by(owner_id=str(user['owner_id'])).first()
-    candidate_id = session.query(Users).filter_by(owner_id=str(candidate['owner_id'])).first()
-    new_favorite_user = Favorites(user_id=user_id.user_id, 
-                                  favorite_user_id=candidate_id.user_id)
-    session.add(new_favorite_user) 
-    session.commit()
-
-
-
-def add_black_list(user, candidate):
-    user_id = session.query(Users).filter_by(owner_id=str(user['owner_id'])).first()
-    candidate_id = session.query(Users).filter_by(owner_id=str(candidate['owner_id'])).first()
-    new_ignored_user = Blacklist(user_id=user_id.user_id, 
-                                  black_list_id=candidate_id.user_id)
-    session.add(new_ignored_user) 
-    session.commit()
-
-# print(check_users('344025107'))
+    bot = Bot()
+    bot.func_main()
